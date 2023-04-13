a109d29d3d4cfb25f40dc12f81568105d1419218
Matt Dietz <matthew.dietz@gmail.com>
2023-04-13 12:29:24 -0500

    Commit before rework

--- main.go
+++ main.go
@@ -6,20 +6,15 @@ import (
 	"log"
 	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
-const NGRAM_SIZE = 2
+const (
+	ENTROPY_THRESHOLD = 4.0
+)
 
-type NGramMatrix map[rune]map[rune]float64
-
-func characterEntropy(input string) int {
-	counts := make(map[rune]int)
-	for _, c := range input {
-		counts[c]++
-	}
-	return len(counts)
-}
+type NGramMatrix map[string]map[rune]float64
 
 func shannonEntropy(input string) float64 {
 	counts := make(map[rune]int)
@@ -28,29 +23,33 @@ func shannonEntropy(input string) float64 {
 	}
 
 	result := 0.0
-	log2 := math.Log(2)
 	listLen := float64(len(counts))
 	for _, count := range counts {
 		freq := float64(count) / listLen
-		result -= freq * (math.Log(freq) / log2)
+		result -= freq * math.Log(freq)
 	}
 	return result
 }
 
-func ngram(input string, n int) []string {
+func ngram(input string, ngramSize int) []string {
 	var result []string
 	input = strings.ToLower(input)
-	for i := 0; i < len(input)-n+1; i++ {
-		result = append(result, input[i:i+n])
+	for i := 0; i < len(input)-ngramSize+1; i++ {
+		for j := 0; j < ngramSize; j++ {
+			if !validChar(rune(input[i+j])) {
+				continue
+			}
+		}
+		result = append(result, input[i:i+ngramSize])
 	}
 	return result
 }
 
 func validChar(r rune) bool {
-	return r >= 32 && r <= 122
+	return r >= 32 && r <= 126
 }
 
-func markovTrain(path string) (float64, NGramMatrix) {
+func markovTrain(path string, ngramSize int) (float64, NGramMatrix) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -58,39 +57,37 @@ func markovTrain(path string) (float64, NGramMatrix) {
 
 	counts := make(NGramMatrix)
 
-	for i := 32; i <= 122; i++ {
-		counts[rune(i)] = make(map[rune]float64)
-		for j := 32; j <= 122; j++ {
-			counts[rune(i)][rune(j)] = 10
-		}
-	}
-
 	scanner := bufio.NewScanner(file)
+	padding := strings.Repeat(" ", 2)
 	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		ngrams := ngram(line, NGRAM_SIZE)
-		for i := 0; i < len(ngrams)-1; i++ {
-			if !validChar(rune(ngrams[i][0])) || !validChar(rune(ngrams[i][1])) {
-				continue
+		// Padding the string with whitespace helps generalize the probabilities for markov chains
+		line := padding + strings.TrimSpace(scanner.Text()) + padding
+		ngrams := ngram(line, ngramSize)
+		for _, ngram := range ngrams {
+			bigram := ngram[:ngramSize-1]
+			nextChar := rune(ngram[ngramSize-1])
+			if _, ok := counts[bigram]; !ok {
+				counts[bigram] = make(map[rune]float64)
 			}
-
-			counts[rune(ngrams[i][0])][rune(ngrams[i][1])]++
+			counts[bigram][nextChar]++
 		}
 	}
 
 	// normalize
-	for row := range counts {
-		sum := 0.0
-		for col := range counts[row] {
-			sum += float64(col)
+	probs := make(NGramMatrix)
+	for bigram, nextCharCounts := range counts {
+		total := 0.0
+		for _, count := range nextCharCounts {
+			total += count
 		}
-		for col := range counts[row] {
-			counts[row][col] = math.Log(counts[row][col] / sum)
+		probs[bigram] = make(map[rune]float64)
+		for char, count := range nextCharCounts {
+			probs[bigram][char] = math.Log(count / total)
 		}
 	}
 
-	goodProbs := calculateProbabilities("good.txt", counts)
-	badProbs := calculateProbabilities("bad.txt", counts)
+	goodProbs := calculateProbabilities("good.txt", probs)
+	badProbs := calculateProbabilities("bad.txt", probs)
 
 	minGood := goodProbs[0]
 	for _, prob := range goodProbs {
@@ -107,83 +104,106 @@ func markovTrain(path string) (float64, NGramMatrix) {
 	}
 
 	// TODO save the model to disk
-	threshold := (minGood + maxBad) / 2
+	threshold := (minGood + maxBad) / 2.0
 
-	fmt.Println(threshold)
 	return threshold, counts
 
 }
 
-func calculateProbabilities(path string, counts NGramMatrix) []float64 {
+func calculateProbabilities(path string, transitionProbs NGramMatrix) []float64 {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var probs []float64
+	var outputProbs []float64
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		probs = append(probs, averageTransitionProbability(line, counts))
+		prob, err := averageTransitionProbability(line, transitionProbs)
+		if err != nil {
+			continue
+		}
+
+		outputProbs = append(outputProbs, prob)
 	}
-	return probs
+	return outputProbs
 }
 
-func averageTransitionProbability(input string, counts NGramMatrix) float64 {
+func averageTransitionProbability(input string, probs NGramMatrix) (float64, error) {
 	logProb := 0.0
-	transitionCt := 0
 
-	for i := 0; i < len(input)-1; i++ {
-		if !validChar(rune(input[i])) || !validChar(rune(input[i+1])) {
-			log.Fatalf("Invalid character in input string %c %c", input[i], input[i+1])
+	ngramSize := 0
+	for k, _ := range probs {
+		ngramSize = len(k) + 1
+		break
+	}
+
+	ngrams := ngram(input, ngramSize)
+	for _, ngram := range ngrams {
+		bigram := ngram[:ngramSize-1]
+		nextChar := rune(ngram[ngramSize-1])
+		if _, ok := probs[bigram]; ok {
+			// The probabilities are stored as log probabilities in the model to avoid underflow
+			if prob, ok := probs[bigram][nextChar]; ok {
+				logProb += prob
+			} else {
+				return 0, fmt.Errorf("No probability for %v", ngram)
+			}
+		} else {
+			return 0, fmt.Errorf("No probability for %v", ngram)
 		}
 	}
 
-	ngrams := ngram(input, 2)
-	for i := 0; i < len(ngrams)-1; i++ {
-		transitionCt++
-		logProb += counts[rune(ngrams[i][0])][rune(ngrams[i+1][1])]
-	}
-
-	if transitionCt == 0 {
-		transitionCt = 1
-	}
-	return math.Exp(logProb / float64(transitionCt))
+	return logProb, nil
 }
 
 // Would this fail for unicode?
-func markovCheck(input string, threshold float64, counts NGramMatrix) bool {
-	return averageTransitionProbability(input, counts) > threshold
+func markovCheck(input string, threshold float64, probs NGramMatrix) bool {
+	prob, err := averageTransitionProbability(input, probs)
+	if err != nil {
+		return false
+	}
+	return prob > threshold
 }
 
 func main() {
-	threshold, counts := markovTrain("big.txt")
-	var testStrings = []string{
-		"Hello, World!",
-		"Hello, World! Hello, World!",
-		"Hello, World! Hello, World! Hello, World!",
-		"abcabc",
-		"abcabcabc",
-		"alksjdlkajsdlkajsd",
-		"crapdad",
-		"crappad",
-		"crappadcrappad",
-		"foobar",
-		"fooooooooobar",
-		"Art Of Craft",
-		"gbxhbncjn",
-		"vcwvbxbxb",
-		"cxtyjei153",
-		"herpdederp.store",
-		"Fadil",
-		"tryu",
-		"sdfsdf",
-		"aaaaaaaaaaabbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaa",
-		"Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch",
+	ngramSize, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	for i := 0; i < len(testStrings); i++ {
-		//fmt.Printf("%s -> linearEntropy score %+v\n", testStrings[i], characterEntropy(testStrings[i]))
-		// fmt.Printf("%s -> shannonEntropy score %+v\n", testStrings[i], shannonEntropy(testStrings[i]))
-		fmt.Printf("%s -> valid? %+v\n", testStrings[i], markovCheck(testStrings[i], threshold, counts))
+	threshold, probs := markovTrain(os.Args[2], ngramSize)
+	readFile, err := os.Open(os.Args[3])
+
+	defer readFile.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	good, total := 0, 0
+
+	for fileScanner.Scan() {
+		line := strings.TrimSpace(fileScanner.Text())
+		if strings.ToLower(line) == "null);" {
+			continue
+		}
+
+		valid := markovCheck(line, threshold, probs)
+		if valid {
+			good++
+			fmt.Println(line)
+		} else {
+			//fmt.Println(line)
+		}
+		total++
+		// ent := shannonEntropy(line)
+		// fmt.Printf("entropy(%v:%v) markov(%v) -> %+v\n", ent, ent > ENTROPY_THRESHOLD, valid, line)
+	}
+	fmt.Printf("Good: %d, Total: %d, Accuracy: %f", good, total, float64(good)/float64(total))
+
 }
